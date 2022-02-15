196724972ea2aeab680a6111f0ae2bbf436b1c38
Iacopo Pazzaglia <iacopo.p.es@sync.money>
2022-02-15 17:26:26 +0100

    create new project and add ModuleServices as dependency

---
+++ GenericModulesCrash/SceneDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  SceneDelegate.swift
+//  GenericModulesCrash
+//
+//  Created by Iacopo Pazzaglia on 15/2/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+
+    }
+
+
+}
+
---
+++ GenericModulesCrash/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenericModulesCrash
+//
+//  Created by Iacopo Pazzaglia on 15/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
