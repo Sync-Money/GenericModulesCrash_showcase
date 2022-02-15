0a46c89bd346f3d870c5ec4da7a3b263b1d4e6bb
Iacopo Pazzaglia <iacopo.p.es@sync.money>
2022-02-15 18:16:34 +0100

    Use correct Bundle.module instead of default nil

--- UIModules/Sources/UIModules/DummyModule.swift
+++ UIModules/Sources/UIModules/DummyModule.swift
@@ -2,4 +2,12 @@
 import UIKit
 import ModuleGenericServices
 
-public class DummyModule: ConfigurableSingleClassRowModule<DummyModuleCell, DummyModuleDecorator> { }
+public class DummyModule: ConfigurableSingleClassRowModule<DummyModuleCell, DummyModuleDecorator> {    
+    open override func registerViews() {
+        [DummyModuleCell.self].forEach { currentClass in
+            let identifier = String(describing: currentClass)
+            let nib = UINib(nibName: identifier, bundle: Bundle.module)
+            tableView.register(nib, forCellReuseIdentifier: identifier)
+        }
+    }
+}
